f07d0327adc19c6f8540b5f9072a635a1570a11f
xyxdbp2022 <xyxdbp2022@gmail.com>
2022-05-14 15:10:17 +0800

    init

---
+++ src/main/java/com/example/jenkins/controller/JenkinsController.java
@@ -0,0 +1,18 @@
+package com.example.jenkins.controller;
+
+import com.example.jenkins.entity.Jenkins;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+
+    @GetMapping("/hello")
+    private String hello(){
+        Jenkins jenkins = new Jenkins();
+        jenkins.setName("wugipeng");
+        return jenkins.getName();
+    }
+}
---
+++ src/main/java/com/example/jenkins/entity/Jenkins.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.entity;
+
+public class Jenkins {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
